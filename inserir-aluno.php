962da5d07832cb2bbfa2a294e3d06376d4ccaab1
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-15 09:22:22 -0300

    Add prepared statement

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,8 +7,19 @@ require_once 'vendor/autoload.php';
 $caminhaBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminhaBanco);
 
-$student = new Student(null, 'Bob Dylan', new \DateTimeImmutable('1997-10-15'));
+$student = new Student(null, 'Bob Dylan 3', new \DateTimeImmutable('1997-10-15'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
 
-var_dump($pdo->exec($sqlInsert));
+// $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+// $statement = $pdo->prepare($sqlInsert);
+// $statement->bindValue(1, $student->name());
+// $statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue('name', $student->name()); 
+$statement->bindValue('birth_date', $student->birthDate()->format('Y-m-d'));
+
+if ($statement->execute()) {
+    echo 'Aluno incluido' . PHP_EOL;
+}
