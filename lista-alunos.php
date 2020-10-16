558d45d73edc5aa45578182c82f315c100f0f501
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-16 17:30:59 -0300

    Updata lista-alunos

--- lista-alunos.php
+++ lista-alunos.php
@@ -2,13 +2,19 @@
 
 use Alura\Pdo\Domain\Model\Student;
 use Alura\Pdo\Infra\Persistence\ConnectionCreator;
+use Alura\Pdo\Infra\Repository\PdoStudentRepository;
 
 require_once 'vendor/autoload.php';
 
 $pdo = ConnectionCreator::createConnection();
+$repository = new PdoStudentRepository($pdo);
+$studentList = $repository->allStudents();
 
-$statement = $pdo->query('SELECT * FROM students;');
-$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+var_dump($studentList);
+
+
+// $statement = $pdo->query('SELECT * FROM students;');
+// $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 // var_dump($statement->fetchColumn(1)); exit();
 // var_dump($statement->fetchAll(PDO::FETCH_CLASS, Student::class));
 
@@ -19,9 +25,9 @@ $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 // }
 // exit();
 
-$studentList = [];
-foreach ($studentDataList as $studentData) {
-    $studentList[] = new Student($studentData['id'], $studentData['name'], new DateTimeImmutable($studentData['birth_date']));
-}
+// $studentList = [];
+// foreach ($studentDataList as $studentData) {
+//     $studentList[] = new Student($studentData['id'], $studentData['name'], new DateTimeImmutable($studentData['birth_date']));
+// }
 
-var_dump($studentList);
+// var_dump($studentList);
