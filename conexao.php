4194797363692cf03bb750f6197946f21a3ad4d3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-16 17:30:26 -0300

    Create Phone

--- conexao.php
+++ conexao.php
@@ -2,9 +2,30 @@
 
 $caminhaBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminhaBanco);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+echo 'Connected: ' . $caminhaBanco . PHP_EOL;
 
-echo 'conn: ' . $caminhaBanco . PHP_EOL;
+$pdo->exec("INSERT INTO phones (area_code, number, student_id) VALUES ('41', '999999999', 1);");
+echo 'Created' . PHP_EOL;
 
-$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
+exit();
 
-echo 'created' . PHP_EOL;
+$createTableSql = '
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        birth_date TEXT
+    );
+    
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY (student_id) REFERENCES students(id)
+    );
+';
+$pdo->exec($createTableSql);
+
+echo 'Created' . PHP_EOL;
